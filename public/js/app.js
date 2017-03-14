fb3127fb895e8c1594934b95fce6b815271faa6d
Johnny Mast <mastjohnny@gmail.com>
2017-03-14 23:49:20 +0100

    Added bulma to package.json.

--- public/js/app.js
+++ public/js/app.js
@@ -31622,7 +31622,7 @@ var Component = __webpack_require__(35)(
   /* cssModules */
   null
 )
-Component.options.__file = "/Users/steve/devapps/sites/myio/resources/assets/js/components/Example.vue"
+Component.options.__file = "C:\\Users\\johnny\\Documents\\myio\\resources\\assets\\js\\components\\Example.vue"
 if (Component.esModule && Object.keys(Component.esModule).some(function (key) {return key !== "default" && key !== "__esModule"})) {console.error("named exports are not supported in *.vue files.")}
 if (Component.options.functional) {console.error("[vue-loader] Example.vue: functional components are not supported with templates, they should use render functions.")}
 
