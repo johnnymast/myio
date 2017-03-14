e1f6fcbf6a63f612b0bf7d678506b07539dd29bd
Steve Popoola <stevepop@gmail.com>
2017-03-14 20:14:27 +0000

    Added Bulma CSS Library via npm Implemented Layout for Frontend page with partials to render header nav and footer

---
+++ resources/views/layouts/admin/admin.blade.php
@@ -0,0 +1,356 @@
+<!DOCTYPE html>
+<html>
+<head>
+  <meta charset="utf-8">
+  <meta http-equiv="X-UA-Compatible" content="IE=edge">
+  <meta name="viewport" content="width=device-width, initial-scale=1">
+  <title>Admin - Free Bulma template</title>
+  <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/font-awesome/4.5.0/css/font-awesome.min.css">
+  <link rel="stylesheet" type="text/css" href="../css/bulma.css">
+  <link rel="stylesheet" href="../demo/morris.css">
+  <link rel="stylesheet" type="text/css" href="../css/admin.css">
+</head>
+<body>
+  <div class="columns">
+    <aside class="column is-2 aside hero is-fullheight is-hidden-mobile">
+      <div>
+        <div class="main">
+          <div class="title">Main</div>
+          <a href="#" class="item active"><span class="icon"><i class="fa fa-home"></i></span><span class="name">Dashboard</span></a>
+          <a href="#" class="item"><span class="icon"><i class="fa fa-map-marker"></i></span><span class="name">Activity</span></a>
+          <a href="#" class="item"><span class="icon"><i class="fa fa-th-list"></i></span><span class="name">Timeline</span></a>
+          <a href="#" class="item"><span class="icon"><i class="fa fa-folder-o"></i></span><span class="name">Folders</span></a>
+        </div>
+      </div>
+    </aside>
+    <div class="column is-10 admin-panel">
+      <nav class="nav has-shadow" id="top">
+        <div class="container">
+          <div class="nav-left">
+            <a class="nav-item" href="../index.html">
+              <img src="../images/bulma.png" alt="Description">
+            </a>
+          </div>
+          <span class="nav-toggle">
+            <span></span>
+            <span></span>
+            <span></span>
+          </span>
+          <div class="nav-right nav-menu is-hidden-tablet">
+            <a href="#" class="nav-item is-active">
+              Dashboard
+            </a>
+            <a href="#" class="nav-item">
+              Activity
+            </a>
+            <a href="#" class="nav-item">
+              Timeline
+            </a>
+            <a href="#" class="nav-item">
+              Folders
+            </a>
+          </div>
+        </div>
+      </nav>
+      <section class="hero is-small">
+        <!-- Hero content: will be in the middle -->
+        <div class="hero-body">
+          <div class="container">
+            <h1 class="title">
+              Admin Dashboard
+            </h1>
+            <h2 class="subtitle">
+              A simple admin template
+            </h2>
+          </div>
+        </div>
+
+        <!-- Hero footer: will stick at the bottom -->
+        <div class="hero-foot">
+          <nav class="level">
+            <div class="level-item has-text-centered">
+              <p class="heading">Tweets</p>
+              <p class="title">3,456</p>
+            </div>
+            <div class="level-item has-text-centered">
+              <p class="heading">Following</p>
+              <p class="title">123</p>
+            </div>
+            <div class="level-item has-text-centered">
+              <p class="heading">Followers</p>
+              <p class="title">456K</p>
+            </div>
+            <div class="level-item has-text-centered">
+              <p class="heading">Likes</p>
+              <p class="title">789</p>
+            </div>
+          </nav>
+        </div>
+      </section>
+      <section class="section">
+        <div class="columns is-mobile is-multiline">
+          <div class="column is-one-third-desktop is-full-mobile">
+            <section class="panel">
+              <p class="panel-heading">
+                Total Users
+              </p>
+              <p class="panel-tabs">
+                <a class="is-active" href="#">All Time</a>
+                <a href="#">Past week</a>
+                <a href="#">Past month</a>
+                <a href="#">Past quarter</a>
+                <a href="#">Past Year</a>
+              </p>
+              <div class="panel-block">
+                <div id="chart1" style="height: 250px;"></div>
+              </div>
+              <div class="panel-block">
+                <button class="button is-default is-outlined is-fullwidth">
+                  View Data
+                </button>
+              </div>
+            </section>
+          </div>
+          <div class="column is-one-third-desktop is-full-mobile">
+            <section class="panel">
+              <p class="panel-heading">
+                Sales
+              </p>
+              <div class="panel-block">
+                <div id="chart2" style="height: 280px;"></div>
+              </div>
+              <div class="panel-block">
+                <button class="button is-default is-outlined is-fullwidth">
+                  View Data
+                </button>
+              </div>
+            </section>
+          </div>
+          <div class="column is-one-third-desktop is-full-mobile">
+            <section class="panel">
+              <p class="panel-heading">
+                YoY Comparison
+              </p>
+              <div class="panel-block">
+                <div id="chart3" style="height: 280px;"></div>
+              </div>
+              <div class="panel-block">
+                <button class="button is-default is-outlined is-fullwidth">
+                  View Data
+                </button>
+              </div>
+            </section>
+          </div>
+          <div class="column is-half-desktop is-full-mobile">
+            <section class="panel">
+              <p class="panel-heading">
+               Notifications
+             </p>
+             <div class="panel-block">
+              <div class="notification is-warning">
+                <button class="delete" onclick="((this).parentNode.remove())"></button>
+                <strong>Server 101a is at 90% disk capacity.</strong>
+                <br>
+                <small>1h ago | via: system</small>
+              </div>
+              <div class="notification">
+                <button class="delete" onclick="((this).parentNode.remove())"></button>
+                <strong>Cron job successfully completed.</strong>
+                <br>
+                <small>2h ago | via: system</small>
+              </div>
+              <div class="notification">
+                <button class="delete" onclick="((this).parentNode.remove())"></button>
+                <strong>Completed automated backup.</strong>
+                <br>
+                <small>1d ago | via: system</small>
+              </div>
+              <div class="notification">
+                <button class="delete" onclick="((this).parentNode.remove())"></button>
+                <strong>Security scan complete. 0 exceptions found.</strong>
+                <br>
+                <small>1d ago | via: system</small>
+              </div>
+            </div>
+          </section>
+        </div>
+        <div class="column is-half-desktop is-full-mobile">
+          <section class="panel">
+            <p class="panel-heading">
+              Forecast
+            </p>
+            <div class="panel-block">
+              <div id="chart5" style="height: 280px;"></div>
+            </div>
+          </section>
+        </div>
+        <div class="column is-half-desktop is-full-mobile">
+          <section class="panel">
+            <p class="panel-heading">
+              Popular Followers
+            </p>
+            <div class="panel-block">
+              <table class="table">
+                <thead>
+                  <tr>
+                    <th>Name</th>
+                    <th>Instrument</th>
+                    <th></th>
+                    <th></th>
+                  </tr>
+                </thead>
+                <tfoot>
+                  <tr>
+                    <th>Name</th>
+                    <th>Instrument</th>
+                    <th></th>
+                    <th></th>
+                  </tr>
+                </tfoot>
+                <tbody>
+                  <tr>
+                    <td>Misty Abbott</td>
+                    <td>Bass Guitar</td>
+                    <td class="is-icon">
+                      <a href="#">
+                        <i class="fa fa-twitter"></i>
+                      </a>
+                    </td>
+                    <td class="is-icon">
+                      <a href="#">
+                        <i class="fa fa-instagram"></i>
+                      </a>
+                    </td>
+                  </tr>
+                  <tr>
+                    <td>John Smith</td>
+                    <td>Rhythm Guitar</td>
+                    <td class="is-icon">
+                      <a href="#">
+                        <i class="fa fa-twitter"></i>
+                      </a>
+                    </td>
+                    <td class="is-icon">
+                      <a href="#">
+                        <i class="fa fa-instagram"></i>
+                      </a>
+                    </td>
+                  </tr>
+                  <tr>
+                    <td>Robert Mikels</td>
+                    <td>Lead Guitar</td>
+                    <td class="is-icon">
+                      <a href="#">
+                        <i class="fa fa-twitter"></i>
+                      </a>
+                    </td>
+                    <td class="is-icon">
+                      <a href="#">
+                        <i class="fa fa-instagram"></i>
+                      </a>
+                    </td>
+                  </tr>
+                  <tr>
+                    <td>Karyn Holmberg</td>
+                    <td>Drums</td>
+                    <td class="is-icon">
+                      <a href="#">
+                        <i class="fa fa-twitter"></i>
+                      </a>
+                    </td>
+                    <td class="is-icon">
+                      <a href="#">
+                        <i class="fa fa-instagram"></i>
+                      </a>
+                    </td>
+                  </tr>
+                </tbody>
+              </table>
+            </div>
+          </section>
+        </div>
+        <div class="column">
+          <section class="panel">
+            <p class="panel-heading">
+             Message User
+           </p>
+           <div class="panel-block">
+            <label class="label">Name</label>
+            <p class="control">
+              <input class="input" type="text" placeholder="Text input">
+            </p>
+            <label class="label">Username</label>
+            <p class="control has-icon has-icon-right">
+              <input class="input is-success" type="text" placeholder="Text input" value="bulma">
+              <i class="fa fa-check"></i>
+              <span class="help is-success">This username is available</span>
+            </p>
+            <label class="label">Email</label>
+            <p class="control has-icon has-icon-right">
+              <input class="input is-danger" type="text" placeholder="Email input" value="hello@">
+              <i class="fa fa-warning"></i>
+              <span class="help is-danger">This email is invalid</span>
+            </p>
+            <label class="label">Subject</label>
+            <p class="control">
+              <span class="select">
+                <select>
+                  <option>Select dropdown</option>
+                  <option>With options</option>
+                </select>
+              </span>
+            </p>
+            <label class="label">Message</label>
+            <p class="control">
+              <textarea class="textarea" placeholder="Textarea"></textarea>
+            </p>
+            <p class="control">
+              <label class="checkbox">
+                <input type="checkbox">
+                Remember me
+              </label>
+            </p>
+            <p class="control">
+              <label class="radio">
+                <input type="radio" name="question">
+                Yes
+              </label>
+              <label class="radio">
+                <input type="radio" name="question">
+                No
+              </label>
+            </p>
+            <p class="control">
+              <button class="button is-primary">Submit</button>
+              <button class="button is-link">Cancel</button>
+            </p>
+          </div>
+        </section>
+      </div>
+    </div>
+  </section>
+</div>
+</div>
+<footer class="footer">
+  <div class="container">
+    <div class="has-text-centered">
+      <p>
+        <strong>Bulma</strong> by <a href="http://jgthms.com">Jeremy Thomas</a>. The source code is licensed
+        <a href="http://opensource.org/licenses/mit-license.php">MIT</a>. The website content
+        is licensed <a href="http://creativecommons.org/licenses/by-nc-sa/4.0/">CC ANS 4.0</a>.
+      </p>
+      <p>
+        <a class="icon" href="https://github.com/jgthms/bulma">
+          <i class="fa fa-github"></i>
+        </a>
+      </p>
+    </div>
+  </div>
+</footer>
+<script async type="text/javascript" src="../js/bulma.js"></script>
+<script type="text/javascript" src="../demo/jquery.js"></script>
+<script type="text/javascript" src="../demo/raphael.min.js"></script>
+<script type="text/javascript" src="../demo/morris.js"></script>
+<script type="text/javascript" src="../demo/admin.js"></script>
+</body>
+</html>
