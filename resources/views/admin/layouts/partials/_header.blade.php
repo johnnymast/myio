4ea5dd0ecb47cce4f36a9bb59f2daec4d116dbbd
Rick Bolton <rickbolton@outlook.com>
2017-03-16 21:33:13 +0000

    Updated header to add Auth check. Updated app name to MyIO

--- resources/views/admin/layouts/partials/_header.blade.php
+++ resources/views/admin/layouts/partials/_header.blade.php
@@ -6,17 +6,31 @@
                     {{ config('app.name', 'MyIO') }}
                 </a>
             </div>
-            <div class="nav-right is-flex">
-                <span class="nav-item">Welcome, {{ \Auth::user()->name }}!</span>
-                <span class="nav-item">
-                    <a href="{{ route('logout') }}" onclick="event.preventDefault();document.getElementById('logout-form').submit();" class="button is-primary">
-                        <span>Logout</span>
-                    </a>
-                    <form id="logout-form" action="{{ route('logout') }}" method="POST" style="display: none;">
-                        {{ csrf_field() }}
-                    </form>
-                </span>
-            </div>
+            @if ( \Auth::check() )
+                <div class="nav-right is-flex">
+                    <span class="nav-item">Welcome, {{ \Auth::user()->name }}!</span>
+                    <span class="nav-item">
+                        <a href="{{ route('logout') }}" onclick="event.preventDefault();document.getElementById('logout-form').submit();" class="button is-primary">
+                            <span>Logout</span>
+                        </a>
+                        <form id="logout-form" action="{{ route('logout') }}" method="POST" style="display: none;">
+                            {{ csrf_field() }}
+                        </form>
+                    </span>
+                </div>
+            @else
+                <div class="nav-right is-flex">
+                    <span class="nav-item">
+                        <a href="{{ route('login') }}" class="button is-primary">
+                            <span>Login</span>
+                        </a>
+
+                        <a href="{{ route('register') }}" class="button is-primary">
+                            <span>Register</span>
+                        </a> 
+                    </span>
+                </div>
+            @endif
         </nav>
     </div>
 </section>
