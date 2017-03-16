8948b7806259acbdc40c3da3c278c6d10af07afe
Johnny Mast <mastjohnny@gmail.com>
2017-03-16 23:27:16 +0100

    myio-17 Added the basic link API create/list/delete.

--- config/app.php
+++ config/app.php
@@ -177,6 +177,10 @@ return [
         App\Providers\EventServiceProvider::class,
         App\Providers\RouteServiceProvider::class,
         Collective\Html\HtmlServiceProvider::class,
+
+        /* Dingo A RESTful API package for the Laravel and Lumen frameworks. */
+        Dingo\Api\Provider\LaravelServiceProvider::class,
+
     ],
 
     /*
