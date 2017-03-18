23ae0ad4aaf5c7306cabf18593c8905d5bdc8498
Johnny Mast <mastjohnny@gmail.com>
2017-03-18 16:43:06 +0100

    myio-17 makeBasicAuthHeader in TestCase.php will help testing the API

--- tests/TestCase.php
+++ tests/TestCase.php
@@ -3,8 +3,15 @@
 namespace Tests;
 
 use Illuminate\Foundation\Testing\TestCase as BaseTestCase;
+use App\User;
 
 abstract class TestCase extends BaseTestCase
 {
     use CreatesApplication;
+
+    protected function makeBasicAuthHeader(User $user) {
+        return [
+            'Authorization' => 'Basic '.base64_encode($user->email.':'.$user->api_token)
+        ];
+    }
 }
