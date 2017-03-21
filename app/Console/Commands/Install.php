f6fd2145bb064f563529ba41c105687911e7e229
Johnny Mast <mastjohnny@gmail.com>
2017-03-21 21:48:46 +0100

    myio-34 [1.0] Wrapped up the code, time to check my style.

--- app/Console/Commands/Install.php
+++ app/Console/Commands/Install.php
@@ -24,7 +24,7 @@ class Install extends Command
      * file.
      * @var string
      */
-    protected $envFile = '.env.debug';
+    protected $envFile = '.env';
 
     /**
      * Same environment file.
