3ee0e78b166c2a508e36dd274abd68241cc0d290
Johnny Mast <mastjohnny@gmail.com>
2017-03-21 23:46:17 +0100

    Added a Contributing and issue template.

--- tests/Feature/ExampleTest.php
+++ tests/Feature/ExampleTest.php
@@ -3,9 +3,6 @@
 namespace Tests\Feature;
 
 use Tests\TestCase;
-use Illuminate\Foundation\Testing\WithoutMiddleware;
-use Illuminate\Foundation\Testing\DatabaseMigrations;
-use Illuminate\Foundation\Testing\DatabaseTransactions;
 
 class ExampleTest extends TestCase
 {
@@ -18,6 +15,6 @@ class ExampleTest extends TestCase
     {
         $response = $this->get('/');
 
-        $response->assertStatus(200);
+        $response->assertStatus(302);
     }
 }
