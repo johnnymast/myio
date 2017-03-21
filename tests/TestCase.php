c269d427313fc31b0a501b10d81026bc7b8cb7f8
Johnny Mast <mastjohnny@gmail.com>
2017-03-21 22:45:11 +0100

    myio-17 Added dingo back again.

--- tests/TestCase.php
+++ tests/TestCase.php
@@ -6,5 +6,26 @@ use Illuminate\Foundation\Testing\TestCase as BaseTestCase;
 
 abstract class TestCase extends BaseTestCase
 {
+
     use CreatesApplication;
+
+    protected $authType = 'basic';
+
+    /**
+     * @param null $user
+     * @return array
+     */
+    protected function authHeader($user = null)
+    {
+        if ( ! $user) {
+            $user = User::first();
+        }
+        if ($this->authType == 'basic') {
+            return [
+                'Authorization' => 'Basic '.base64_encode($user->email.':'.$user->api_token)
+            ];
+        }
+
+        return [];
+    }
 }
