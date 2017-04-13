606a83d38fcc8ab89a48e465203082953fb67c2c
Steve Popoola <stevepop@gmail.com>
2017-04-13 21:22:00 +0100

    If user is authenticated but not admin, user is redirected to logged in home route

--- app/Http/Middleware/SecureAdmin.php
+++ app/Http/Middleware/SecureAdmin.php
@@ -36,9 +36,7 @@ class SecureAdmin
 
         // Allow access only to administrators
         if (! $this->auth->user()->hasRole('Administrator')) {
-            abort(404, 'You are not authorized to view requested resource.');
-
-            return redirect()->route('homepage');
+            return redirect()->route('home');
         }
 
         return $next($request);
